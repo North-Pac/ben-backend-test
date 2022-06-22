cc37c2d4d1309a4eb9049b39b5f9603f5bbd3dc2
Benjamin <benj.james.carter@gmail.com>
2022-06-21 20:47:18 -0700

    added procfile, added add_photo post method

--- main.py
+++ main.py
@@ -48,6 +48,33 @@ async def get_all_photos():
     return formatted_photos
 
 
+@app.post("/photos", status_code=201)
+async def add_photo(file: UploadFile):
+    print("Endpoint hit!!")
+    print(file.filename)
+    print(file.content_type)
+
+    # Upload file to AWS S3
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(S3_BUCKET_NAME)
+    bucket.upload_fileobj(file.file, file.filename,
+                          ExtraArgs={"ACL": "public-read"})
+
+    uploaded_file_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{file.filename}"
+
+    # Store URL in database
+    conn = psycopg2.connect(
+        database="irodb", user="docker", password="docker", host="0.0.0.0"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        f"INSERT INTO photo (photo_name, photo_url) VALUES ('{file.filename}', '{uploaded_file_url}' )"
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
 @app.get("/test")
 async def root():
     return {"message": "Hello, world!"}
